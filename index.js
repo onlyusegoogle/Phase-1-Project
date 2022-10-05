010260755704d4ef4bfb45fb87419e476ff37999
Yana <yana.videoeditor.gmail.com>
2022-10-05 10:36:01 -0600

    added Stripe form and CSS

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const coinList = document.getElementById('list') // -> change it for table places
-//const stripe = Stripe("pk_test_51LpDh5GTck0sdjJJB2jBz7La6RAo7wJrML0EEenMPzyNdHVGGgxv8kc5vEv9Wy9yy9wjY7AIEnz4Jo2zj8Tgdjvv00SyIxzIDb");
 
+const stripe = Stripe("pk_test_51LpDh5GTck0sdjJJB2jBz7La6RAo7wJrML0EEenMPzyNdHVGGgxv8kc5vEv9Wy9yy9wjY7AIEnz4Jo2zj8Tgdjvv00SyIxzIDb");
 
-fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&fbclid=IwAR2seyvVJZbkupG8ZP75m6uh_cqGH5NeTLcq2l4v7cAPKCPF_jh-_Kvc9dg") //{mode: "no-cors"})
+//iFrame
+const appearance = {
+    theme: 'default',
+  };
+elements = stripe.elements({appearance, clientSecret: 'pi_3LpN6OGTck0sdjJJ3qk2PDzl_secret_PZ4EI0W264HiqirIrVmTOUE06'});
+const paymentElement = elements.create("payment");
+paymentElement.mount("#payment-element");
+
+fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&fbclid=IwAR2seyvVJZbkupG8ZP75m6uh_cqGH5NeTLcq2l4v7cAPKCPF_jh-_Kvc9dg")//, {mode: "no-cors"})
     .then(Response => Response.json())
     .then(coinArr => {
         for (let i = 0; i < 7; i++) {
@@ -15,12 +23,9 @@ fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&fbclid=IwA
 
 function displayInfo(coinObj) {
     // console.log(coinObj)
-    //console.log(coinObj[0])
+    //-> doesn't work: console.log(coinObj[0])
     // console.log(coinObj.name)
-    // console.log(coinObj.symbol)
     // console.log(coinObj.image)
-    // console.log(coinObj.current_price)
-    // console.log(coinObj.price_change_percentage_24h)
 
     //->same as 
     //console.log(coinObj.name, coinObj.current_price, coinObj.image, coinObj.symbol)
@@ -30,9 +35,13 @@ function displayInfo(coinObj) {
     // x- is key
 
     const tr = document.createElement('tr')
-    // //TODO: add star img. create Favorit colomn
+    //TODO: add star img. created Favorit colomn
     let td = document.createElement('td')
     tr.append(td)
+    // const emptyStar = document.createElement('img')
+    // starFavorit.addEventListener ('click', () => toggleStar(coinObj))
+    // td.append(emptyStar)
+    // TODO emptyStar.srs = .
 
     // added coin Img
     td = document.createElement('td')
@@ -40,6 +49,11 @@ function displayInfo(coinObj) {
     const coinImg = document.createElement('img')
     td.append(coinImg)
     coinImg.src = coinObj.image
+
+    //added symbol
+    td = document.createElement('td')
+    tr.append(td)
+    td.textContent = coinObj.symbol
 
     //added coin Name
     td = document.createElement('td')
@@ -51,7 +65,7 @@ function displayInfo(coinObj) {
     tr.append(td)
     td.textContent = coinObj.current_price
 
-    //added persantage
+    //added percantage
     td = document.createElement('td')
     tr.append(td)
     td.textContent = coinObj.price_change_percentage_24h
@@ -62,5 +76,19 @@ function displayInfo(coinObj) {
 
     coinList.append(tr);
 }
+//toggle star Favorite
+//const myWatched = document.getElementById ("watched")
+// let currMovie;
+
+// function showMovie (movieObj) {
+//     currMovie = movieObj;
+// myWatched.textContent = movieObj.watched? "watched": "unwatched";
+// }
+
+// function toggleStar () {
+//     currMovie.watched = !currMovie.watched;
+//     myWatched.textContent = currMovie.watched? "watched": "unwatched";
+// }
 
 
+// myWatched.addEventListener ('click', toggleStar)
