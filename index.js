ff46681cec9dc388db7367d80155df2828896f5f
Yana <yana.videoeditor.gmail.com>
2022-10-06 11:24:14 -0600

    added events: dblclick, click

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const stripe = Stripe("pk_test_51LpDh5GTck0sdjJJB2jBz7La6RAo7wJrML0EEenMPzyNdHVG
 //iFrame
 const appearance = {
     theme: 'default',
-  };
-elements = stripe.elements({appearance, clientSecret: 'pi_3LpN6OGTck0sdjJJ3qk2PDzl_secret_PZ4EI0W264HiqirIrVmTOUE06'});
+};
+elements = stripe.elements({ appearance, clientSecret: 'pi_3LpN6OGTck0sdjJJ3qk2PDzl_secret_PZ4EI0W264HiqirIrVmTOUE06' });
 const paymentElement = elements.create("payment");
 paymentElement.mount("#payment-element");
 
+//API coin
 fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&fbclid=IwAR2seyvVJZbkupG8ZP75m6uh_cqGH5NeTLcq2l4v7cAPKCPF_jh-_Kvc9dg")//, {mode: "no-cors"})
+    // TODO -> fetch(('http://localhost:3000/market'))
     .then(Response => Response.json())
     .then(coinArr => {
         for (let i = 0; i < 7; i++) {
@@ -22,7 +24,7 @@ fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&fbclid=IwA
 // .then(coinArr => console.log(coinArr))
 
 function displayInfo(coinObj) {
-    // console.log(coinObj)
+    // -> console.log(coinObj)
     //-> doesn't work: console.log(coinObj[0])
     // console.log(coinObj.name)
     // console.log(coinObj.image)
@@ -33,62 +35,53 @@ function displayInfo(coinObj) {
     // check it on w3schools: for in, for of
     // for (x in obj)
     // x- is key
-
     const tr = document.createElement('tr')
-    //TODO: add star img. created Favorit colomn
-    let td = document.createElement('td')
-    tr.append(td)
-    // const emptyStar = document.createElement('img')
-    // starFavorit.addEventListener ('click', () => toggleStar(coinObj))
-    // td.append(emptyStar)
-    // TODO emptyStar.srs = .
 
-    // added coin Img
+    // -> add star img. created Favorit colomn
+    td = document.createElement('td')
+    tr.append(td)
+    let emptyStar = document.createElement('img')
+    emptyStar.src = './img/empty_star.png'
+    // -> td.setAttribute('id', 'starButton')
+    td.id = 'starButton'
+    td.append(emptyStar)
+    // -> Double click event and click event
+    emptyStar.addEventListener("dblclick", () => {
+        emptyStar.src = './img/empty_star.png'
+        emptyStar.src = './img/filled_star.png'
+        console.log("empty")
+    })
+    emptyStar.addEventListener("click", () => {
+        emptyStar.src = './img/filled_star.png'
+        emptyStar.src = './img/empty_star.png'
+        console.log("emptyfiled")
+    })
+
+    // -> added coin Img
     td = document.createElement('td')
     tr.append(td)
     const coinImg = document.createElement('img')
     td.append(coinImg)
     coinImg.src = coinObj.image
 
-    //added symbol
+    // -> added symbol
     td = document.createElement('td')
     tr.append(td)
     td.textContent = coinObj.symbol
 
-    //added coin Name
+    // -> added coin Name
     td = document.createElement('td')
     tr.append(td)
     td.textContent = coinObj.name
 
-    // added prices
+    // -> added prices
     td = document.createElement('td')
     tr.append(td)
     td.textContent = coinObj.current_price
 
-    //added percantage
+    // -> added percantage
     td = document.createElement('td')
     tr.append(td)
     td.textContent = coinObj.price_change_percentage_24h
-
-    // This made an ERROR:
-    // td = document.createElement('td')
-    // tr.textContent = coinObj.price_change_percentage_24h
-
     coinList.append(tr);
 }
-//toggle star Favorite
-//const myWatched = document.getElementById ("watched")
-// let currMovie;
-
-// function showMovie (movieObj) {
-//     currMovie = movieObj;
-// myWatched.textContent = movieObj.watched? "watched": "unwatched";
-// }
-
-// function toggleStar () {
-//     currMovie.watched = !currMovie.watched;
-//     myWatched.textContent = currMovie.watched? "watched": "unwatched";
-// }
-
-
-// myWatched.addEventListener ('click', toggleStar)
